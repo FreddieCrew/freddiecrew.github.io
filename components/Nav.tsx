ebcc88f485c7496c9cdc17c4847b880ca7397167
John Michael Reyes <120303127+FreddieCrew@users.noreply.github.com>
2023-09-15 13:32:14 -0500

    Add Nav component

---
+++ components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <footer className="bg-none text-white py-2">
+      <div className="flex">
+        <ul className='gap-4 ml-8 inline-flex items-center text-center list-none'>
+            <li><a className='hover:underline hover:opacity-50' href='/'>Home</a></li>
+            <li><a className='hover:underline hover:opacity-50' href='/about'>About me</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Nav;
