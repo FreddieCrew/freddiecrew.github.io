c37235969ae32f7aa8c0582dcaad4ac007cba9bf
John Michael Reyes <120303127+FreddieCrew@users.noreply.github.com>
2023-09-15 13:32:55 -0500

    Update index page to include navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Footer from '../../components/Footer';
 import Head from 'next/head';
 import Header from '../../components/header';
 import Toast from '../../components/Toast';
+import Navbar from '../../components/Nav';
 
 export default function Home() {
 
   return (
     <div className='root'>
       <Toast />
+      <Navbar />
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
