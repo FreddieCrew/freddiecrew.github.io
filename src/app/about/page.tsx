d92ede65800aa1d422766f0d8aa90cac025a862b
John Michael Reyes <120303127+FreddieCrew@users.noreply.github.com>
2023-09-15 13:33:20 -0500

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import Footer from '../../../components/Footer';
+import Navbar from '../../../components/Nav';
+
+export default function About() {
+
+  const divstyles = {
+    overflow: 'hidden',
+  }
+
+  return (
+    <div className='about' style={divstyles}>
+      <Navbar />
+      <p className='text-center text-white py-3'>
+        Hi there, my name is John Michael Reyes, and I was born in El Paso Texas, but I currently live in Houston, Texas.
+      </p>
+      <p className='text-white text-center'>
+        I currently work as a Hardware Engineer. Although, I'm also a full-stack web developer. However I'm more skilled in front-end but I can handle back-end as well.
+      </p>
+      <p className='text-center text-white m-2'>
+        Some of my side projects include <a title='Simple Character Counter written in C++, which can count how many lines are in a .txt file.' href='https://github.com/FreddieCrew/CharCounter' className='text-indigo-50 underline hover:no-underline hover:opacity-50' target='_blank'>CharCounter</a> and <a title='Simple password generator written in Python' href='https://github.com/FreddieCrew/Password-Generator' className='text-indigo-50 underline hover:no-underline hover:opacity-50'>PasswordGenerator</a>.
+      </p>
+
+      
+    </div>
+  )
+}
