6fbcff6d4f8ee39ee1aba7bd0e3d78b84bf201b2
John M. Reyes <120303127+FreddieCrew@users.noreply.github.com>
2023-12-11 23:51:07 -0600

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
         Hi there, my name is John Michael Reyes, and I was born in El Paso, Texas, but I currently live in Houston, Texas.
       </p>
       <p className='ml-10 text-white'>
-        I currently work as a Hardware Engineer, and I'm also a full-stack web developer. Although I'm more skilled in front-end development, but I can handle back-end as well.
+        I work as a Hardware Engineer and am also a full-stack web developer. Although I'm more skilled in front-end development, I can handle back-end as well.
       </p>
       <p className='ml-10 text-white m-2'>
         Some of my side projects include <a title='Simple Character Counter written in C++, which can count how many lines are in a .txt file.' href='https://github.com/FreddieCrew/CharCounter' className='text-indigo-50 no-underline hover:underline hover:opacity-80' target='_blank'>CharCounter</a>, and <a title='Simple password generator written in Python' href='https://github.com/FreddieCrew/Password-Generator' className='text-indigo-50 no-underline hover:underline hover:opacity-80'>PasswordGenerator</a>.
